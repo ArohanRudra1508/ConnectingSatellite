0fa2b100ff3f36ba1a68c7679d6a3a3210915970
ArohanRudra1508 <msanchi08@gmail.com>
2024-09-03 17:00:56 +0200

    Update satteliteconnector.py

--- satteliteconnector.py
+++ satteliteconnector.py
@@ -10,7 +10,7 @@ HEIGHT = 600
 
 starttime = 0
 endtime = 0
-totaltime = 0
+total_time = 0
 
 next_satellite = 0
 number_of_satellite = 12
@@ -23,12 +23,11 @@ def create_satellite():
     for i in range(0,number_of_satellite):
         sat = Actor('sattelite')
         sat.pos = randint(50,WIDTH-50),randint(50,HEIGHT-50)
-        pass
         satellites.append(sat)
     starttime = time()
 
 def draw():
-    global totaltime
+    global total_time
     screen.blit('satbg',(0,0))
     n = 1
     for sat in satellites:
@@ -40,9 +39,19 @@ def draw():
         screen.draw.line(line[0], line[1], (255,255,255))
     if next_satellite < number_of_satellite:
         total_time = time() - starttime
-        screen.draw.text(str(total_time),(350,450),fontsize = 20)
+        screen.draw.text(str(round(total_time,2)),(350,450),fontsize = 20)
     else:
-        screen.draw.text(str(total_time),(350,450),fontsize = 20)
+        screen.draw.text(str(round(total_time,2)),(350,450),fontsize = 20)
+
+def on_mouse_down(pos):
+    global next_satellite, lines
+    if next_satellite <  number_of_satellite:
+        if satellites[next_satellite].collidepoint(pos):
+            if next_satellite:
+                lines.append((satellites[next_satellite -1].pos,satellites[next_satellite].pos))
+        else:
+            next_satellite = 0
+            lines = 0
 
 create_satellite()
 
